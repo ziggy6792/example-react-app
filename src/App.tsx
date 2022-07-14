cc0b518270d662715087da1159c6f92015b81d02
Simon Verhoeven <simon.verhoeven@telepathy.ai>
2022-07-14 13:44:57 +0700

    fetch users

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Suspense } from 'react';
 import './App.css';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
 
-function App() {
+const queryClient = new QueryClient();
+
+const UserList: React.FC = () => {
+  const { data } = useQuery('users', () => fetch('http://localhost:4000/users').then((res) => res.json()), { suspense: true });
+
+  return <div>{JSON.stringify(data)}</div>;
+};
+
+const Users: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <UserList />
+    </Suspense>
   );
-}
+};
+
+const App: React.FC = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Users />
+    </QueryClientProvider>
+  );
+};
 
 export default App;
