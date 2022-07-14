1ca6a958487dad4070e7f7c7e926839a2a261f20
Simon Verhoeven <simon.verhoeven@telepathy.ai>
2022-07-14 13:45:37 +0700

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,18 @@ import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
 
 const queryClient = new QueryClient();
 
-const UserList: React.FC = () => {
+const Users: React.FC = () => {
   const { data } = useQuery('users', () => fetch('http://localhost:4000/users').then((res) => res.json()), { suspense: true });
 
   return <div>{JSON.stringify(data)}</div>;
 };
 
-const Users: React.FC = () => {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <UserList />
-    </Suspense>
-  );
-};
-
 const App: React.FC = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <Users />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Users />
+      </Suspense>
     </QueryClientProvider>
   );
 };
